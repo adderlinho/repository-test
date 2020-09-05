149e187192246621e84e4ce07f3d85789f7b8291
Adderly Sinay <addersinay@gmail.com>
2020-09-05 08:18:19 -0600

    Actualización de la clase Greeting
    Se colocó una nueva clase con mi número de carné.

--- src/main/java/gt/edu/umg/test/contoller/Greeting.java
+++ src/main/java/gt/edu/umg/test/contoller/Greeting.java
@@ -8,13 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
-public class Greeting7690137664 {
+public class Greeting {
 	
 	@GetMapping("/hello")
 	public String getGreeting() {
 		Date date = new Date();
 		//esto solo debe aparecer en testing
 		return "Hello world " + date.toString();
+	}
+
+}
+
+@RestController
+@RequestMapping("/greeting7690137664")
+public class Greeting7690137664 {
+	
+	@GetMapping("/hello")
+	public String getGreeting() {
+		Date date = new Date();
+		//esto solo debe aparecer en testing
+		return "Hello Adderly" + date.toString();
 	}
 
 }
